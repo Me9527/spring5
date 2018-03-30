435e74221fa334cbd059aabd04a622b2743f4141
mike <mike@mike-PC>
2018-03-30 23:51:20 +0800

    d

--- spring4study/src/main/webapp/html/modules/module02/login.js
+++ spring4study/src/main/webapp/html/modules/module02/login.js
@@ -2,52 +2,65 @@
 $(document).ready(function() {
   $('#loginModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-	  //var modal = $(this)
-	  //modal.find('.modal-title').text('New message to ' + recipient)
-	  //modal.find('.modal-body input').val(recipient)
+    var recipient = button.data('whatever') // Extract info from data-*
+											// attributes
+	  // If necessary, you could initiate an AJAX request here (and then do
+		// the updating in a callback).
+	  // Update the modal's content. We'll use jQuery here, but you could use
+		// a data binding library or other methods instead.
+	  // var modal = $(this)
+	  // modal.find('.modal-title').text('New message to ' + recipient)
+	  // modal.find('.modal-body input').val(recipient)
   })
 
   $("#doLoginBtn").click(function(event){
     // event.preventDefault();
-    //openModel();
-    //modal.find('.modal-body input').val("登录中。。。");
-    ajaxGet();
+    // openModel();
+    // modal.find('.modal-body input').val("登录中。。。");
+	  ajaxLogin();
   })
   
   $("#testAccessBtn").click(function(event){
 	  testAccessRefuse();
   })
   
-
+  $("#logoutBtn").click(function(event){
+	  ajaxLogout();
+  })
   
 
   
 })
 
 // DO GET X-CSRF-TOKEN
-function ajaxGet(){
-  var url = '/spring4study/modules/user/getCsrf.jsp';
-  $.ajax({
+function ajaxGetCsrfToken(){
+	var url = '/spring4study/modules/user/getCsrf.jsp';
+	var token = null;
+	$.ajax({
   	   type: "GET",
+  	   async :false,
   	   timeout : 20000,
        url : url,
        success: function(data){
-         //alert(data);
-         ajaxPost(data);
-        },
-        error : function(e) {
-          alert(e);
-        }
-    }); 
+    	   // alert(data);
+    	   token = data;
+       },
+       error : function(err) {
+    	   // alert(err);
+       }
+    });
+	return token;
 }
   
-//  'application/json;charset=utf-8' application/json
-function ajaxPost(data){
+// 'application/json;charset=utf-8' application/json
+function ajaxLogin(){
+  var data = ajaxGetCsrfToken();
+  if(null == data){
+	  alert("Get CsrfToken Error!");
+	  return;
+  }
   var queryParam = {};
-  //alert($("#username").val());
+  // alert($("#username").val());
   queryParam['username'] = $("#inputUsername").val();
   queryParam['password'] = $("#inputPassword").val();	
   queryParam['targetUrl'] = "/html/modules/module02/loginSuccess.json";	
@@ -65,54 +78,79 @@ function ajaxPost(data){
     url        : url,
     success    : function(data){
         // fill data to Modal Body
-        //fillData(data);
-        //alert(data);
+        // fillData(data);
+        // alert(data);
     	if(data.success == false){
     		alert(data.message);
     	}else if (data.success == true){
-        	$("#showLoginBtn").text("注销");
+        	//$("#showLoginBtn").text("注销");
             $("#logedUser").text("孙悟空");
+            $('#loginModal').modal('hide')
+            //$("#showLoginBtn").click(function(event){
+            //	ajaxLogout();
+            //})
     	}
 
      },
      error     : function(e) {
-          	//fillData(null);
+          	// fillData(null);
        }
    }); 
 }
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-////////////////////////////////////////////////////////////////////////////
-function testAccessRefuse(){
-  	var url = '/spring4study/modules/user/getCsrf.jsp';
-  	$.ajax({
-  	    type: "GET",
-  	    timeout : 20000,
-          url : url,
-          success: function(data){
-          	//alert(data);
-        	  doTest(data);
-          },
-          error : function(e) {
-          	alert(e);
-          }
-      }); 
-  }
+function ajaxLogout(){
+	  var data = ajaxGetCsrfToken();
+	  if(null == data){
+		  alert("Get CsrfToken Error!");
+		  return;
+	  }
+	  var queryParam = {};
+	  var url = '/spring4study//modules/user/logout';
+	  $.ajax({
+	 	contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+	  	headers    : {
+	  	             'X-CSRF-TOKEN': data,
+	  	 },
+	  	dataType   : 'json',
+	  	type       : "POST",
+	  	timeout    : 20000,
+	  	data       : queryParam,
+	    url        : url,
+	    success    : function(data){
+	        // fill data to Modal Body
+	        // fillData(data);
+	        // alert(data);
+	    	if(data.success == false){
+	    		alert(data.message);
+	    	}else if (data.success == true){
+	            $("#logedUser").text("");
+	            alert(data.message);
+	    	}
 
-function doTest(data){
+	     },
+	     error     : function(e) {
+	          	// fillData(null);
+	       }
+	   }); 
+	}  
+  
+  
+  
+  
+  
+  
+// //////////////////////////////////////////////////////////////////////////
+
+function testAccessRefuse(){
+  	var data = ajaxGetCsrfToken();
+  	if(null == data){
+		alert("Get CsrfToken Error!");
+		return;
+  	}
   	var queryParam = {};
-  	//alert($("#username").val());
-    //"/modules/module02/ActionTwo/aabbcc.do?${_csrf.parameterName}=${_csrf.token}";
+  	// alert($("#username").val());
+    // "/modules/module02/ActionTwo/aabbcc.do?${_csrf.parameterName}=${_csrf.token}";
   	var url = '/spring4study/modules/module02/ActionTwo/aabbcc.do';
   	$.ajax({
  		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -134,4 +172,4 @@ function doTest(data){
         		alert(err.message);
         }
     }); 
-  }
+}
