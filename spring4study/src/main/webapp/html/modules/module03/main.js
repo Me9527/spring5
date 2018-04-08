923e001b16c1deafc3be19d7103899335b6b3814
CPSDC <CPSDC@192.168.10.201>
2018-04-08 16:27:54 +0800

    d

--- spring4study/src/main/webapp/html/modules/module03/main.js
+++ spring4study/src/main/webapp/html/modules/module03/main.js
@@ -80,64 +80,65 @@ $(document).ready(function() {
 //                </ul>                
 //              </div>
 //           </div>
-           
+
 function makeMenu(){
-	var menuSections = [];
-	menuSections.push('动态菜单');// menuSections.push('动态菜单');
-	
-	var nodes = [];
-	nodes.push('node1'); nodes.push('node2'); nodes.push('node3');
-	
-	for(var i=0; i<menuSections.length; i++){
-		$('#sidebar-menu').append("<div class='menu_section' id='menu_section-" + menuSections[i] + i + "'/>");
-		//var elem = "'#" + menuSections[i] + i + "'";
-		var menuSection = $('#' + 'menu_section-' + menuSections[i] + i );
-		menuSection.append("<h3>" + menuSections[i] + "_" + (i+1) + "</h3>");
-		
-		menuSection.append("<ul class='nav side-menu' id='side-menu-" + menuSections[i] + i + "'/>");
-		var sideMenu = $('#' + 'side-menu-' + menuSections[i] + i );
-		sideMenu.append("<li id='side-menu-li-" + menuSections[i] + i + "'/>");
-		var sideMenuLi = $('#' + 'side-menu-li-' + menuSections[i] + i );
-		sideMenuLi.append("<a><i class='fa fa-table'></i>" +  menuSections[i] + "_" + (i+1) + "<span class='fa fa-chevron-down'></span></a>");
-		
-		sideMenuLi.append("<ul class='nav child_menu' id='t_child_menu-" + menuSections[i] + i + "'/>");
-		var child_menu = $('#' + 't_child_menu-' + menuSections[i] + i );
-		for(var i=0; i<nodes.length; i++){
-			child_menu.append("<li><a href='javascript:void(0)' onclick='clickOnNode(\"" + nodes[i] + "\")'>" + nodes[i] + "</a></li>");
-			//var nodeElement = "<li><a href='tables_dynamic.html'>" + nodes[i] + "</a></li>";
-		}
-		
-	}
-	init_sidebar();
+	var sidebarMenuData = ajaxGetSidebarMenuData();
+	//nodes 层次数必须>=2,与菜单展示形式有关。
+	var nodes = sidebarMenuData.nodes;
+
+	$('#sidebar-menu').append("<div class='menu_section' id='menu_section_01" + "'/>");
+	var menuSection = $('#' + 'menu_section_01');
+	menuSection.append("<h3>" + sidebarMenuData.title + "</h3>");
+	menuSection.append("<ul class='nav side-menu' id='side_menu_01" + "'/>");
+	var menuContain = $('#' + 'side_menu_01');	
 
 	
-//	
-//	$('#sidebar-menu').append("<li><a href='tables.html'>c1</a></li>");
-//	
-//	var menu = $('#myMenu');
-//	$('#sidebar-menu').append("<div class='menu_section' id='ment1'> <h3>我的动态菜单</h3> ");
-//	//$('#sidebar-menu').append("<h3>我的动态菜单</h3>");
-//	$('#ment1').append("<ul class='nav side-menu' id='ment2'>");
-//	
-//	$('#ment2').append("<li id='ment3'><a><i class='fa fa-table'></i> aaa <span class='fa fa-chevron-down'></span></a> <ul class='nav child_menu' id='ment4'> <li><a href='tables.html'>b1</a></li> <li><a href='tables_dynamic.html'>b2</a></li> </ul> </li>");
-//	
-//	$('#ment2').append("<li id='ment3'><a><i class='fa fa-table'></i> aaa <span class='fa fa-chevron-down'></span></a>");
-//	$('#ment3').append("<ul class='nav child_menu' id='ment4'>");
-//	$('#ment4').append("<li><a href='tables.html'>b1</a></li>");
-//	$('#ment4').append("<li><a href='tables_dynamic.html'>b2</a></li>");
-//	$('#ment3').append("</ul>");
-//	$('#ment2').append("</li>");
-//
-//	$('#ment2').append("<li id='ment5'><a><i class='fa fa-table'></i> bbb <span class='fa fa-chevron-down'></span></a>");
-//	$('#ment5').append("<ul class='nav child_menu' id='ment6'>");
-//	$('#ment6').append("<li><a href='tables.html'>c1</a></li>");
-//	$('#ment6').append("<li><a href='tables_dynamic.html'>c2</a></li>");
-//	$('#ment5').append("</ul>");
-//	$('#ment2').append("</li>");
-//	
-//	$('#ment1').append("</ul>");
-//	$('#sidebar-menu').append("</div>");
-//	
+	for(var i=0; i<nodes.length; i++){	
+		menuContain.append("<li id='side-menu-li-" + nodes[i].id + "'/>");
+		var topNode = $('#' + 'side-menu-li-' + nodes[i].id );
+		topNode.append("<a><i class='fa fa-sitemap'></i>" +  nodes[i].name+ "<span class='fa fa-chevron-down'></span></a>");
+		
+		rescureMakeMenu(nodes[i], topNode);
+	}
+	init_sidebar();
+}
+
+function rescureMakeMenu(node, parentNode){
+	var children = node.children;
+	if(null != children && children != "undefined" && children.length > 0){
+		parentNode.append("<ul class='nav child_menu' id='t_child_menu-" + node.id + "'/>");
+		var child_menu = $('#' + 't_child_menu-' + node.id );
+		for(var k=0; k<children.length; k++){
+			var childrenChildren = children[k].children;
+			if(null != childrenChildren && childrenChildren != "undefined" && childrenChildren.length > 0){
+				child_menu.append("<li id='child-menu-li-" + children[k].id + "'/>");
+				var child_child_menu = $('#' + 'child-menu-li-' + children[k].id );
+				child_child_menu.append("<a>" +  children[k].name+ "<span class='fa fa-chevron-down'></span></a>");
+				rescureMakeMenu(children[k], child_child_menu);
+			}else{
+				child_menu.append("<li><a href='javascript:void(0)' onclick='clickOnNode(\"" + children[k].name + "\")'>" + children[k].name + "</a></li>");
+			}
+		}
+	}
+}
+
+function ajaxGetSidebarMenuData(){
+	var url = '/spring4study/html/modules/module03/menus.json';
+	var sidebarMenuData = null;
+	$.ajax({
+  	   type: "GET",
+  	   async :false,
+  	   timeout : 20000,
+       url : url,
+       success: function(data){
+    	   // alert(data);
+    	   sidebarMenuData = data;
+       },
+       error : function(err) {
+    	   // alert(err);
+       }
+    });
+	return sidebarMenuData;
 }
 
 function clickOnNode( value){
