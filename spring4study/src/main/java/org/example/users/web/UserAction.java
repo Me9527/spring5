2d6b2dcda7dbd6a82a33f842fc151e6b218147aa
mike <mike@mike-PC>
2018-04-10 22:35:08 +0800

    e

--- spring4study/src/main/java/org/example/users/web/UserAction.java
+++ spring4study/src/main/java/org/example/users/web/UserAction.java
@@ -1,10 +1,16 @@
 package org.example.users.web;
 
+import javax.servlet.http.HttpSession;
+
+import org.example.framework.json.JsonResult;
 import org.example.users.services.IUserService;
+import org.example.users.util.UserConstants;
+import org.example.users.vo.UserInfoVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserAction {
@@ -15,14 +21,20 @@ public class UserAction {
 	public String getAllUser(@RequestParam(value = "username", required = false, defaultValue = "钟馗") String username,
 			Model model) {
 
-		/*
-		 * model.addAttribute("username", username); System.out.println("page02");
-		 * bizTwo.addBiz01(username); 
-		 * 
-		 * @SuppressWarnings("unused") List<Map<String, Object>> rs =
-		 * bizTwo.getBiz01(username);
-		 */
 		return "page02";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/getUserInfo.do")
+	public JsonResult getUserInfo(HttpSession session) {
+//		UserInfoVO user = (UserInfoVO)session.getAttribute(UserConstants.UserInfoInHttpSession);
+		
+		Object obj = session.getAttribute(UserConstants.UserInfoInHttpSession);
+		if(obj != null && obj instanceof UserInfoVO)
+			return new JsonResult(true, "success", obj);
+		else
+			return new JsonResult(false, "Not login");
+		
 	}
 	
 	public IUserService getUserService() {
