9e24251f4c1c42ba212c834015ab1d4fa1187763
CPSDC <CPSDC@192.168.10.201>
2018-04-13 16:56:26 +0800

    d

--- spring4study/src/main/webapp/html/modules/module03/main.js
+++ spring4study/src/main/webapp/html/modules/module03/main.js
@@ -56,9 +56,12 @@ $(document).ready(function() {
 		  backColor: 'SteelBlue'
 	});
 	
-    $('#example').DataTable( {
+    $('#datatable-buttons').DataTable( {
         "ajax": 'arrays.txt'
     } );
+    
+    init_DataTables();
+    
 //  $("#doLoginBtn").click(function(event){
 //	  ajaxLogin();
 //  })
@@ -71,6 +74,92 @@ $(document).ready(function() {
   
 })
 
+			
+			function init_DataTables() {
+				
+				console.log('run_datatables');
+				
+				if( typeof ($.fn.DataTable) === 'undefined'){ return; }
+				console.log('init_DataTables');
+				
+				var handleDataTableButtons = function() {
+				  if ($("#datatable-buttons").length) {
+					$("#datatable-buttons").DataTable({
+					  dom: "Blfrtip",
+					  buttons: [
+						{
+						  extend: "copy",
+						  className: "btn-sm"
+						},
+						{
+						  extend: "csv",
+						  className: "btn-sm"
+						},
+						{
+						  extend: "excel",
+						  className: "btn-sm"
+						},
+						{
+						  extend: "pdfHtml5",
+						  className: "btn-sm"
+						},
+						{
+						  extend: "print",
+						  className: "btn-sm"
+						},
+					  ],
+					  responsive: true
+					});
+				  }
+				};
+
+				TableManageButtons = function() {
+				  "use strict";
+				  return {
+					init: function() {
+					  handleDataTableButtons();
+					}
+				  };
+				}();
+
+				$('#datatable').dataTable();
+
+				$('#datatable-keytable').DataTable({
+				  keys: true
+				});
+
+				$('#datatable-responsive').DataTable();
+
+				$('#datatable-scroller').DataTable({
+				  ajax: "js/datatables/json/scroller-demo.json",
+				  deferRender: true,
+				  scrollY: 380,
+				  scrollCollapse: true,
+				  scroller: true
+				});
+
+				$('#datatable-fixed-header').DataTable({
+				  fixedHeader: true
+				});
+
+				var $datatable = $('#datatable-checkbox');
+
+				$datatable.dataTable({
+				  'order': [[ 1, 'asc' ]],
+				  'columnDefs': [
+					{ orderable: false, targets: [0] }
+				  ]
+				});
+				$datatable.on('draw.dt', function() {
+				  $('checkbox input').iCheck({
+					checkboxClass: 'icheckbox_flat-green'
+				  });
+				});
+
+				TableManageButtons.init();
+				
+			};
+			
 function logout(){
 	var url = '/spring4study/modules/user/logout';
 	$.ajax({
