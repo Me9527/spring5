89d196400b0b81261975479bc028cfa37ada5743
mike <mike@mike-PC>
2018-04-16 21:45:21 +0800

    e

--- spring4study/src/main/webapp/html/modules/module03/main.js
+++ spring4study/src/main/webapp/html/modules/module03/main.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
 		  backColor: 'SteelBlue'
 	});
 	
-//    $('#datatable-buttons').DataTable( {
+//    $('#exampleDatatable').DataTable( {
 //        "ajax": 'arrays.txt'
 //    } );
 //    
@@ -75,90 +75,87 @@ $(document).ready(function() {
 })
 
 			
-			function init_DataTables() {
-				
-				console.log('run_datatables');
-				
-				if( typeof ($.fn.DataTable) === 'undefined'){ return; }
-				console.log('init_DataTables');
-				var handleDataTableButtons = function() {
-				  if ($("#datatable-buttons").length) {
-					$("#datatable-buttons").DataTable({
-					  dom: "Blfrtip",
-					  "ajax": 'arrays.txt',
-					  buttons: [
-						{
-						  extend: "copy",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "csv",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "excel",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "pdfHtml5",
-						  className: "btn-sm"
-						},
-						{
-						  extend: "print",
-						  className: "btn-sm"
-						},
-					  ],
-					  responsive: true
-					});
-				  }
-				};
+function init_DataTables() {
 
-				TableManageButtons = function() {
-				  "use strict";
-				  return {
-					init: function() {
-					  handleDataTableButtons();
-					}
-				  };
-				}();
+	console.log('run_datatables');
+	if (typeof ($.fn.DataTable) === 'undefined') {
+		return;
+	}
+	console.log('init_DataTables');
+	
+	var handleDataTableButtons = function() {
+		if ($("#exampleDatatable").length) {
+			$("#exampleDatatable").DataTable({
+				dom : "Blfrtip",
+				"ajax" : 'arrays.txt',
+				buttons : [ {
+					extend : "copy",
+					className : "btn-sm"
+				}, {
+					extend : "csv",
+					className : "btn-sm"
+				}, {
+					extend : "excel",
+					className : "btn-sm"
+				}, {
+					extend : "pdfHtml5",
+					className : "btn-sm"
+				}, {
+					extend : "print",
+					className : "btn-sm"
+				}, ],
+				responsive : true
+			});
+		}
+	};
 
-				$('#datatable').dataTable();
+	TableManageButtons = function() {
+		"use strict";
+		return {
+			init : function() {
+				handleDataTableButtons();
+			}
+		};
+	}();
 
-				$('#datatable-keytable').DataTable({
-				  keys: true
-				});
+//	$('#datatable').dataTable();
+//
+//	$('#datatable-keytable').DataTable({
+//		keys : true
+//	});
 
-				$('#datatable-responsive').DataTable();
+//	$('#datatable-responsive').DataTable();
 
-				$('#datatable-scroller').DataTable({
-				  ajax: "js/datatables/json/scroller-demo.json",
-				  deferRender: true,
-				  scrollY: 380,
-				  scrollCollapse: true,
-				  scroller: true
-				});
+//	$('#datatable-scroller').DataTable({
+//		ajax : "js/datatables/json/scroller-demo.json",
+//		deferRender : true,
+//		scrollY : 380,
+//		scrollCollapse : true,
+//		scroller : true
+//	});
 
-				$('#datatable-fixed-header').DataTable({
-				  fixedHeader: true
-				});
+//	$('#datatable-fixed-header').DataTable({
+//		fixedHeader : true
+//	});
 
-				var $datatable = $('#datatable-checkbox');
+//	var $datatable = $('#datatable-checkbox');
+//
+//	$datatable.dataTable({
+//		'order' : [ [ 1, 'asc' ] ],
+//		'columnDefs' : [ {
+//			orderable : false,
+//			targets : [ 0 ]
+//		} ]
+//	});
+//	$datatable.on('draw.dt', function() {
+//		$('checkbox input').iCheck({
+//			checkboxClass : 'icheckbox_flat-green'
+//		});
+//	});
 
-				$datatable.dataTable({
-				  'order': [[ 1, 'asc' ]],
-				  'columnDefs': [
-					{ orderable: false, targets: [0] }
-				  ]
-				});
-				$datatable.on('draw.dt', function() {
-				  $('checkbox input').iCheck({
-					checkboxClass: 'icheckbox_flat-green'
-				  });
-				});
+	TableManageButtons.init();
 
-				TableManageButtons.init();
-				
-			};
+};
 			
 function logout(){
 	var url = '/spring4study/modules/user/logout';
